9ebe83b17938ebffca218c7c66af0823f7ce355b
brainee <zhengyong145@163.com>
2019-11-24 17:09:11 +0800

    om-copy code v1

---
+++ config.js
@@ -0,0 +1,10 @@
+
+var baseInput="/Users/brain/workhome/app/cw3";
+var baseDest="/Users/brain/workhome/app/cw3-copy";
+module.exports = {
+    source:baseInput+"/**", 
+    destination:baseDest, 
+    exclude:baseInput+"/node_modules/**"
+    // exclude:"**node_modules"
+
+};
---
+++ start.js
@@ -0,0 +1,3 @@
+var om=require("./index");
+
+om.copyWithConfig();
